cf163c42b358adca796c3154a1892e33d3f8d306
Inhandui Barbosa Ferreira <inhandui@gmail.com>
2019-11-28 01:00:48 -0300

    Adding screen elements reference.

--- app/src/main/java/com/cursoandroid/corpersonalizada/MainActivity.java
+++ app/src/main/java/com/cursoandroid/corpersonalizada/MainActivity.java
@@ -3,12 +3,32 @@ package com.cursoandroid.corpersonalizada;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup radioGroup;
+    private RadioButton radioButton; //selected radio button
+    private Button btnSalvar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        radioGroup = findViewById(R.id.radioGroup);
+
+        btnSalvar = findViewById(R.id.btnSalvar);
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
