d21c575d99ba42a342e80c61b7b8e1a3d6eec0b2
Inhandui Barbosa Ferreira <inhandui@gmail.com>
2019-11-28 02:18:37 -0300

    Adding screen color preference action.

--- app/src/main/java/com/cursoandroid/corpersonalizada/MainActivity.java
+++ app/src/main/java/com/cursoandroid/corpersonalizada/MainActivity.java
@@ -1,23 +1,41 @@
 package com.cursoandroid.corpersonalizada;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ConstraintLayout layout;
+
     private RadioGroup radioGroup;
     private RadioButton radioButton; //selected radio button
+    private int idRadio;
     private Button btnSalvar;
+
+    private static final String ARQUIVO_PRERENCIA = "ArqPreferencia";
+
+    private String corEscolhida;
+
+    private static final String MEU_AZUL = "#FF2196F3";
+    private static final String MEU_LARANJA = "#FFFF5722";
+    private static final String MEU_VERDE = "#FF009688";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout = findViewById(R.id.layout);
 
         radioGroup = findViewById(R.id.radioGroup);
 
@@ -26,9 +44,39 @@ public class MainActivity extends AppCompatActivity {
         btnSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                idRadio = radioGroup.getCheckedRadioButtonId();
+                if (idRadio > 0){
+                    radioButton = findViewById(idRadio);
+
+                    SharedPreferences sharedPreferences = getSharedPreferences(ARQUIVO_PRERENCIA, 0);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                    corEscolhida = radioButton.getText().toString();
+
+                    editor.putString("corEscolhida", corEscolhida);
+                    editor.commit();
+                    setBackground(corEscolhida);
+
+                }
+                else {
+                    Toast.makeText(MainActivity.this, R.string.erro, Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
+    }
 
+    private void setBackground(String cor) {
+        switch (cor){
+            case "Azul":
+                layout.setBackgroundColor(Color.parseColor(MEU_AZUL));
+                break;
+            case "Laranja":
+                layout.setBackgroundColor(Color.parseColor(MEU_LARANJA));
+                break;
+            case "Verde":
+                layout.setBackgroundColor(Color.parseColor(MEU_VERDE));
+                break;
+        }
     }
 }
